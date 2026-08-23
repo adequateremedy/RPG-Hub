8dcaf9a122356a828d21883dcb74b6eefd125e4a
adequateremedy <157491572+adequateremedy@users.noreply.github.com>
2026-08-23 13:45:30 -0500

    Delete js/updates.js

--- js/updates.js
+++
@@ -1,9 +0,0 @@
-export const CURRENT_HUB_VERSION = "1.0.4";
-
-export const SYSTEM_UPDATES = [
-    {
-        id: "update_004_image_system",
-        date: "2026-08-23", // The Hub will auto-delete this 14 days from this date!
-        imageUrl: "assets/images/updates/Update-v1.0.4.png" // Exactly matching your uploaded image
-    }
-];
